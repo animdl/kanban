8ab886e466ff03fe4236350009f6d7fe80994910
Animdl <animdl.gh@gmail.com>
2024-12-30 16:35:48 -0600

    added list drag and drop functionality
    allows for lists reordering using dnd-kit drag and drop library

--- src/components/BoardController.tsx
+++ src/components/BoardController.tsx
@@ -1,14 +1,33 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 import PlusIcon from "./icons/PlusIcon";
 import { List, ID } from "../types";
 import ListContainer from "./ListContainer";
+import { DndContext, DragEndEvent, DragOverlay, DragStartEvent, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
+import { createPortal } from "react-dom";
 
 function BoardController() {
     
     // stores the lists of the board and the function to update the lists
     // makes them of type List from the types file
     const [lists, setLists] = useState<List[]>([]);
+
+    // stores the ids of the lists
+    // useMemo is used to cache the list of ids between re-renders
+    const listIDs = useMemo(() => lists.map((list) => list.id), [lists]);
+
+    // stores the list that is currently being dragged
+    const [activeList, setActiveList] = useState<List | null>(null);
+
+    // sets the activation distance of the onDragStart event
+    const sensors = useSensors(useSensor(PointerSensor, {
+        activationConstraint: {
+            distance: 10
+        }
+    }));
+
+    // ---------
 
     // function to add a new list
     function addNewList() {
@@ -24,8 +43,33 @@ function BoardController() {
         return Date.now().toString();
     }
 
+    // function to delete a list
+    // this function is passed to the ListContainer component
     function deleteList(id: ID) {
         setLists(lists.filter((list) => list.id !== id));
+    }
+
+    // function to handle the drag start event
+    // if the dragged item is a list, set activeList to the list
+    function onDragStart(event: DragStartEvent) {
+        if (event.active.data.current?.type === "list") {
+            setActiveList(event.active.data.current.list);
+            return;
+        }
+    }
+
+    // function to handle the drag end event
+    // used to persist the order of the lists after the drag
+    function onDragEnd(event: DragEndEvent) {
+        const { active, over } = event;
+
+        if (active.id !== over.id) {
+            setLists((lists) => {
+                const oldIndex = lists.findIndex((list) => list.id === active.id);
+                const newIndex = lists.findIndex((list) => list.id === over.id);
+                return arrayMove(lists, oldIndex, newIndex);
+            });
+        }
     }
 
     return (
@@ -38,30 +82,43 @@ function BoardController() {
             overflow-x-hidden
             overflow-y-hidden
             p-8">
-            <div className="flex gap-4">
-                {/* lists */}
+            <DndContext onDragStart={onDragStart} onDragEnd={onDragEnd} sensors={sensors}>
                 <div className="flex gap-4">
-                    {lists.map((list) => (
-                        <div>
-                            <ListContainer list={list} deleteList={deleteList}/>
-                        </div>
-                    ))}
+                    {/* display the lists */}
+                    <div className="flex gap-4">
+                        <SortableContext items={listIDs}>
+                            {lists.map((list) => (
+                                <div>
+                                    <ListContainer key={list.id} list={list} deleteList={deleteList}/>
+                                </div>
+                            ))}
+                        </SortableContext>
+                    </div>
+                    {/* button to add a new list */}
+                    <button className="
+                        btn 
+                        btn-outline
+                        h-12
+                        w-[300px]
+                        min-w-[300px]
+                        border-4
+                        btn-primary"
+                        onClick={() => {addNewList()}}>
+                        {/* button content */}
+                        <PlusIcon />
+                        Add a List
+                    </button>
                 </div>
-                {/* button to add a new list */}
-                <button className="
-                    btn 
-                    btn-outline
-                    h-12
-                    w-[300px]
-                    min-w-[300px]
-                    border-4
-                    btn-primary"
-                    onClick={() => {addNewList()}}>
-                    {/* button content */}
-                    <PlusIcon />
-                    Add a List
-                </button>
-            </div>
+                
+                {/* drag overlay appears when a list is being dragged */}
+                {createPortal(
+                    <DragOverlay>
+                        {activeList && (
+                            <ListContainer list={activeList} deleteList={deleteList} />
+                        )}
+                    </DragOverlay>, document.body
+                )}
+            </DndContext>
         </div>
     );
 }
--- src/components/ListContainer.tsx
+++ src/components/ListContainer.tsx
@@ -2,6 +2,9 @@ import { List, ID } from "../types";
 
 import TrashIcon from "./icons/TrashIcon";
 
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
 interface Props {
     list: List;
     deleteList: (id: ID) => void;
@@ -10,8 +13,28 @@ interface Props {
 function ListContainer(props: Props) {
     const { list, deleteList } = props;
 
+    const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
+        id: list.id,
+        data: {
+            type: "list",
+            list
+        }
+    });
+
+    const style = {
+        transition,
+        transform: CSS.Transform.toString(transform)
+    };
+
+    // if the card is being dragged, show a transparent card in its place
+    if (isDragging) {
+        return(
+            <div ref={setNodeRef} style={style} className="card bg-base-300 h-[500px] max-h-[500px] w-[300px] min-w-[300px] flex flex-col rounded-none opacity-40 border-2 border-primary"></div>
+        );
+    }
+
     return (
-        <div className="
+        <div ref={setNodeRef} style={style} className="
             card
             bg-base-300
             h-[500px] 
@@ -23,11 +46,11 @@ function ListContainer(props: Props) {
             rounded-none
             ">
             {/* card header */}
-            <div className="card-title flex items-center justify-between h-16 p-2 cursor-grab">
+            <div {...attributes} {...listeners} className="card-title flex items-center justify-between h-16 p-2 cursor-grab">
                 <div className="p-4">
-                    { list.title }
+                    {list.title}
                 </div>
-                <button className="btn btn-outline btn-error" onClick={() => {deleteList(list.id)}}>    
+                <button className="btn btn-outline btn-error" onClick={() => { deleteList(list.id) }}>
                     <TrashIcon />
                 </button>
             </div>
